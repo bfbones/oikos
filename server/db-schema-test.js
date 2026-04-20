8e042ad9320c1abdf3cdea55708ad0d1a1dc6699
Ulas Kalayci <ulas.kalayci@googlemail.com>
2026-04-20 23:34:41 +0200

    fix(calendar): add missing idx_calendar_sub to db-schema-test MIGRATIONS_SQL[11]

--- server/db-schema-test.js
+++ server/db-schema-test.js
@@ -230,6 +230,7 @@ const MIGRATIONS_SQL = {
     CREATE UNIQUE INDEX idx_calendar_sub_extid
       ON calendar_events (subscription_id, external_calendar_id)
       WHERE subscription_id IS NOT NULL;
+    CREATE INDEX IF NOT EXISTS idx_calendar_sub ON calendar_events(subscription_id);
   `,
 };
 
